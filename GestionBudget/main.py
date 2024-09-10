1fecb8bd3a734722b2a6f46ce5ad4ac127dd8d94
BrunBrun <aubrunpaul49@gmail.com>
2024-09-10 19:29:39 +0200

    Ajout d'une nouvelle fonction permettant d'utiliser la classe TradeRepublicFileExcelJson pour créer un fichier Excel et commence des fichiers JSON

--- GestionBudget/main.py
+++ GestionBudget/main.py
@@ -2,14 +2,13 @@ from RecuperationDesDonnees import DataExtractor
 from TraitementDesDonneesTkinter import OperationCategoriser
 from GraphiquesPlotly import GraphiqueFinancier
 from CreationFichierExcel import ExcelReportGenerator
+from PortefeuilleBoursier import TradeRepublicPerformance
+from TradeRepublicExcelJson import TradeRepublicFileExcelJson
 
-from FonctionsSecondaires import ExtraireAnnee
-from FonctionsSecondaires import DiviserParMois
-from FonctionsSecondaires import SaveDictToJson
-from FonctionsSecondaires import CreerNomFichier
+from FonctionsSecondaires import *
 
 
-def Main():
+def CompteLivret():
     # Créer une instance de DataExtractor
     extracteur = DataExtractor(initialDir="Bilan/Archives")
     
@@ -100,6 +99,80 @@ def Main():
                     chartGen.SetOutputFile(CheminFile)
                     chartGen.GraphiqueAutomatique(compteCourant=False)
 
+def Bourse():
+    directory = f"Bilan/Archives/Bourse/Fichiers pdf/"
+    tickerMapping = {
+        'Hermes': 'RMS.PA',
+        'Air': 'AI.PA',
+        'TotalEnergies': 'TTE.PA',
+        "Oréal": 'OR.PA',
+        'ACCOR': 'AC.PA',
+        'Danone': 'BN.PA',
+        'Pernod Ricard': 'RI.PA',
+        'LVMH': 'MC.PA',
+        'Sodexo': 'SW.PA',
+        'Schneider Electric': 'SU.PA',
+        'Capgemini': 'CAP.PA',
+        'Cap gemini': 'CAP.PA',
+        'iShares Physical Metals PLC': 'PHYMF',
+        'iShs VII-Core S&P': 'CSPX.L',
+        'AbbVie': 'ABBV',
+        'Alphabet': 'GOOGL',
+        'Salesforce': 'CRM',
+        'Amazon': 'AMZN',
+        'Apple': 'AAPL',
+        'Arista Networks': 'ANET',
+        'Blackrock': 'BLK',
+        'Broadcom': 'AVGO',
+        'Cadence Design Systems': 'CDNS',
+        'Coca-Cola': 'KO',
+        'Meta Platforms': 'META',
+        'Fortinet': 'FTNT',
+        'Intuit': 'INTU',
+        'Johnson & Johnson': 'JNJ',
+        'KLA Corp': 'KLAC',
+        'Lam Research': 'LRCX',
+        'Eli Lilly': 'LLY',
+        'Mastercard': 'MA',
+        "McDonald's": 'MCD',
+        'Microsoft': 'MSFT',
+        'Novo-Nordisk': 'NVO',
+        'NVIDIA': 'NVDA',
+        'Oracle': 'ORCL',
+        'Palo Alto Networks': 'PANW',
+        'PepsiCo': 'PEP',
+        'Procter & Gamble': 'PG',
+        'Roper Technologies': 'ROP',
+        'S&P Global': 'SPGI',
+        'Synopsys': 'SNPS',
+        'UnitedHealth': 'UNH',
+        'VISA': 'V',
+        'Walmart': 'WMT',
+        'Cintas': 'CTAS'
+    }
+
+    TradeRepublic = TradeRepublicFileExcelJson(directoryData=directory, tickerMapping=tickerMapping)
+
+    # Liste des opérations à effectuer
+    operations = [
+        {"directory": (directory + "Ordres d'achats/data"), "directoryRename": (directory + "Ordres d'achats"), "createFunction": TradeRepublic.RenameAndMoveOrdresAchats},
+        {"directory": (directory + "Dépôts d'argents/data"), "directoryRename": (directory + "Dépôts d'argents"), "createFunction": TradeRepublic.RenameAndMoveDepotRetraitArgentInteret},
+        {"directory": (directory + "Dividendes/data"), "directoryRename": (directory + "Dividendes"), "createFunction": TradeRepublic.RenameAndMoveDividendes},
+        {"directory": (directory + "Interets/data"), "directoryRename": (directory + "Interets"), "createFunction": TradeRepublic.RenameAndMoveDepotRetraitArgentInteret},
+        {"directory": (directory + "Retraits d'argents/data"), "directoryRename": (directory + "Retraits d'argents"), "createFunction": TradeRepublic.RenameAndMoveDepotRetraitArgentInteret},
+        {"directory": (directory + "Ordres de ventes/data"), "directoryRename": (directory + "Ordres de ventes/FacturesVentes"), "createFunction": TradeRepublic.RenameAndMoveOrdresVentes},
+    ]
+
+    # Boucle pour traiter chaque type de donnée et créer les feuilles Excel correspondantes
+    for operation in operations:
+        TradeRepublic.ProcessPdf(operation["directory"], operation["directoryRename"], operation["createFunction"])
+        
+    TradeRepublic.DownloadDataAndCreateFileExcel("Bilan/Bourse/Récapitulatif des gains.xlsx", False)
+
+    bourse = TradeRepublicPerformance("Bilan/Archives/Bourse/Transactions.json")
+    EnregistrerDataFrameEnJson(bourse.GetEvolutionPrixPortefeuille(False), "Bilan/Archives/Bourse/Portefeuille.json")
+
 
 if __name__  == "__main__":
-    Main()
+    # CompteLivret()
+    Bourse()
